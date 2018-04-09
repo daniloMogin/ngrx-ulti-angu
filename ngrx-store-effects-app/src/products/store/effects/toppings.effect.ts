19558122be496140222f103552b8dc8e52bbc045
Danilo Mogin <danilo.mogin@enlightit.rs>
2018-04-09 22:43:58 +0200

    Further Effects

---
+++ ngrx-store-effects-app/src/products/store/effects/toppings.effect.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+import { Effect, Actions } from '@ngrx/effects';
+import { of } from 'rxjs/observable/of';
+import { map, catchError, switchMap } from 'rxjs/operators';
+
+import * as toppingsActions from '../actions/toppings.action';
+import * as fromService from '../../services/toppings.service';
+
+@Injectable()
+export class ToppingsEffects {
+    constructor(
+        private actions$: Actions,
+        private toppingsService: fromService.ToppingsService
+    ) {}
+
+    @Effect()
+    loadToppings$ = this.actions$.ofType(toppingsActions.LOAD_TOPPINGS).pipe(
+        switchMap(() => {
+            return this.toppingsService
+                .getToppings()
+                .pipe(
+                    map(
+                        toppings =>
+                            new toppingsActions.LoadToppingsuccess(toppings)
+                    ),
+                    catchError(error =>
+                        of(new toppingsActions.LoadToppingsFail(error))
+                    )
+                );
+        })
+    );
+}
